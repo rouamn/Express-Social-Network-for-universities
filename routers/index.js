d1946f53eb34f840cea87904e2408d97ad3ec003
Eya-jomli <eya.jomli@esprit.tn>
2024-04-02 11:29:41 +0200

    firt event backend merge

--- routers/index.js
+++ routers/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const authRoute = require("./auth.js");
 const userRoutes = require("./users.js");
-const postRoute = require("./Post.js")
+const postRoute = require("./Post.js");
+const eventRoute = require("./event.js")
 const router = express.Router();
 
 router.use('/auth', authRoute);
 router.use('/users', userRoutes);
 router.use('/posts', postRoute);
+router.use('/event', eventRoute);
 module.exports = router;
